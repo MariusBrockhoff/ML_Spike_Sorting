18570f874d8e27328b1e4e3206829936fa1a1a3c
Marius Brockhoff <maribrock24@gmail.com>
2023-05-27 12:57:58 +0100

    error fix eval

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -27,10 +27,10 @@ def acc(y_true, y_pred):
 
 def evaluate_clustering(y_pred, y_pred_test, y_true, y_true_test):
     # Split data in kmeans and FHC
-    y_pred_s = y_pred[:int(len(y_pred) / 2), :]
-    y_pred_t = y_pred[int(len(y_pred) / 2):, :]
-    y_pred_test_s = y_pred_test[:int(len(y_pred_test) / 2), :]
-    y_pred_test_t = y_pred_test[int(len(y_pred_test) / 2):, :]
+    y_pred_s = y_pred[:int(len(y_pred) / 2)]
+    y_pred_t = y_pred[int(len(y_pred) / 2):]
+    y_pred_test_s = y_pred_test[:int(len(y_pred_test) / 2)]
+    y_pred_test_t = y_pred_test[int(len(y_pred_test) / 2):]
 
     train_acc_k = acc(y_true, y_pred_s)
     train_acc_f = acc(y_true, y_pred_t)
@@ -50,10 +50,10 @@ def evaluate_clustering(y_pred, y_pred_test, y_true, y_true_test):
 
 def DINO_evaluate_clustering(y_pred, y_pred_test, y_true, y_true_test):
     # Split data in student and teacher
-    y_pred_s = y_pred[:int(len(y_pred) / 2), :]
-    y_pred_t = y_pred[int(len(y_pred) / 2):, :]
-    y_pred_test_s = y_pred_test[:int(len(y_pred_test) / 2), :]
-    y_pred_test_t = y_pred_test[int(len(y_pred_test) / 2):, :]
+    y_pred_s = y_pred[:int(len(y_pred) / 2)]
+    y_pred_t = y_pred[int(len(y_pred) / 2):]
+    y_pred_test_s = y_pred_test[:int(len(y_pred_test) / 2)]
+    y_pred_test_t = y_pred_test[int(len(y_pred_test) / 2):]
 
     train_acc_s = acc(y_true, y_pred_s)
     train_acc_t = acc(y_true, y_pred_t)
