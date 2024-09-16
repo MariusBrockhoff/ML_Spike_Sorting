025ba2a3b6f9421cf8751c836f52a3985219a17f
Jakob Traeuble <jakob@advertise.de>
2024-09-16 18:50:47 +0100

    a

--- PCA_HDBSCAN.py
+++ PCA_HDBSCAN.py
@@ -15,8 +15,8 @@ args = parser.parse_args()
 model_config = Config_DenseAutoencoder()
 
 data_preprocessing_config = Config_Preprocessing(args.PathData)
-pretraining_config = Config_Pretraining(args.PathData)
-fintune_config = Config_Finetuning(args.PathData)
+pretraining_config = Config_Pretraining(args.PathData, model_config.MODEL_TYPE)
+fintune_config = Config_Finetuning(args.PathData, model_config.MODEL_TYPE)
 
 dataset, dataset_test, pretraining_config, fintune_config = data_preparation(model_config=model_config,
                                                                              data_preprocessing_config=data_preprocessing_config,
